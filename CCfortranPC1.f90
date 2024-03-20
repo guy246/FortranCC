d0a8016c6af1a99a16792e41f06cb7ce964a76a4
Gaya Héliès <91890427+guy246@users.noreply.github.com>
2024-03-20 12:47:06 +0100

    Update CCfortranPC1.f90

--- CCfortranPC1.f90
+++ CCfortranPC1.f90
@@ -5,22 +5,20 @@ program Nagaoka
         real function coefficient_a(E, S)
             implicit none
             real, dimension(:), intent(in) :: E, S
-            real, intent(out) :: moym,moys,moyms,moym2 
-            real :: a
+            real :: moym,moys,moyms,moymm 
         end function coefficient_a
 
         real function coefficient_b(E, S)
             implicit none
             real, dimension(:), intent(in) :: E, S
-            real, intent(out) :: moym,moys,moyms,moym2 
-            real :: b
+            real :: moym,moys,moyms,moymm 
         end function coefficient_b
 
-        real function dichotomie(a,c)
-            implicit none
-            real, intent(in) :: a,c
-            real :: f,dl,dlmin,dlmax
-        end function dichotomie
+        !real function dichotomie(a,c)
+        !    implicit none
+        !    real, intent(in) :: a,c
+        !    real :: f,dl,dlmin,dlmax
+        !end function dichotomie
 
     end interface
 
@@ -30,33 +28,34 @@ program Nagaoka
 
     !Définir la taille des tableaux 
     n=21
-    write(*,"(/,'Saisir le nombre de point pour appliquer le nombre de point'/)",advance="no")
-    read(*,*) nb
+    !write(*,"(/,'Saisir le nombre de point pour appliquer le nombre de point'/)",advance="no")
+    !read(*,*) nb
 
     !allouez les tableaux
     allocate(k(n))
     allocate(s(n))
     allocate(D(n))
 
-    ! Définissez les valeurs de k
-	k = (/1.0, 0.818136, 0.688423, 0.595045, 0.52551, 0.471865, 0.429199, 0.394401, 0.365438, &
-	0.340898, 0.319825, 0.301502, 0.285406, 0.271144, 0.258407, 0.246982, 0.236581, 0.227147, 0.218528, &
-	0.210617,0.203315/)
-	
-	! Définissez dlmin et dlmax
-	dlmin = 0
-	dlmax = 10
+    !Définir les valeurs des tableaux
+    k = (/0.818136, 0.788525, 0.760886, 0.735079, 0.710969, 0.688423, 0.667315, 0.647527, 0.62895, &
+    0.611487, 0.595045, 0.579543, 0.564903, 0.551057, 0.537945, 0.52551, 0.513701, 0.502472, 0.491782, &
+    0.481591, 0.471865/)
+
+    !definissez dlmin et dlmax
+    dlmin=0.5
+    dlmax=2.5
 
     !définissez les valeurs de s
-    s=(/(dlmin+(((i-4)*(dlmax-dlmin))/(n-1)),i=1,n)/)
+    s=(/(dlmin+(((i-1)*(dlmax-dlmin))/(n-1)),i=1,n)/)
+    s=s/2
 
     !appelle de la dichotomie
-    dl = dichotomie(a,0)
-    write(*,"('dl = ',f6.3)") dl
+    !dl = dichotomie(a,0)
+    !write(*,"('dl = ',f6.3)") dl
 	
     !appelez la fonction coefficient_a et coefficient_b
-    a = coefficient_a(s,k)
-    b = coefficient_b(s,k)
+    a = coefficient_a(s,1/k)
+    b = coefficient_b(s,1/k)
     write(*,"('a = ',f6.3)") a
     write(*,"('b = ',f6.3)") b
 
@@ -76,40 +75,45 @@ program Nagaoka
 	deallocate(D)
 	
 	stop "fin"
-	contains
-	
-	real function coefficient_a(k,s)
-        moym = sum(k)/size(k)
-        moys = sum(s)/size(s)
-        moyms = sum(k*s)/size(k)
-        moym2 = sum(k*k)/size(k)
-        coefficient_a = (moyms-(moym*moys))/(moym2-(moym*moym))
-    end function coefficient_a
-    real function coefficient_b(k,s)
-        moym = sum(k)/size(k)
-        moys = sum(s)/size(s)
-        moyms = sum(k*s)/size(k)
-        moym2 = sum(k*k)/size(k)
-        coefficient_b = (moys*moym2-moym*moyms)/(moym2-(moym*moym))
-    end function coefficient_b
-
-    !Définir la fonction dichotomie avec a la valeur obtenue précédemment et c la valeur qui permet d'annuler la fonction f qui sera définie dans l'inerval [-0.1,0] ave une précision d'au moins 0.001
-    !f(c)=1/(1+a*(dl/2)+c((dl/2)*(dl/2)))
-    real function dichotomie(a,c)
-        dlmin = 0
-        dlmax = 10
-        dl = (dlmax-dlmin)/2
-        f = 1/(1+a*(dl/2)+c*((dl/2)*(dl/2)))
-        do while (abs(f) > 0.001)
-            if (f > 0) then
-                dlmin = dl
-            else
-                dlmax = dl
-            end if
-            dl = (dlmax+dlmin)/2
-            f = 1/(1+a*(dl/2)+c*((dl/2)*(dl/2)))
-        end do
-        dichotomie = dl
-    end function dichotomie
     
 end program Nagaoka
+
+real function coefficient_a(E,S)
+    implicit none
+    real, dimension(:), intent(in) :: E, S
+    real :: moym,moys,moyms,moymm 
+    moym = sum(E)/size(E)
+    moys = sum(S)/size(S)
+    moyms = sum(S*E)/size(S)
+    moymm = sum(E*E)/size(E)
+    coefficient_a = (moyms-(moym*moys))/(moymm-(moym*moym))
+end function coefficient_a
+
+real function coefficient_b(E,S)
+    implicit none
+    real, dimension(:), intent(in) :: E, S
+    real :: moym,moys,moyms,moymm 
+    moym = sum(E)/size(E)
+    moys = sum(S)/size(S)
+    moyms = sum(S*E)/size(S*E)
+    moymm = sum(E*E)/size(E*E)
+    coefficient_b = (moys*moymm-moym*moyms)/(moymm-(moym*moym))
+end function coefficient_b
+
+    !Définir la fonction dichotomie 
+    !real function dichotomie(a,c)
+    !    dlmin = 0
+    !    dlmax = 10
+    !    dl = (dlmax-dlmin)/2
+    !    f = 1/(1+a*(dl/2)+c*((dl/2)*(dl/2)))
+    !    do while (abs(f) > 0.001)
+    !        if (f > 0) then
+    !            dlmin = dl
+    !        else
+    !            dlmax = dl
+    !        end if
+    !        dl = (dlmax+dlmin)/2
+    !        f = 1/(1+a*(dl/2)+c*((dl/2)*(dl/2)))
+    !    end do
+    !    dichotomie = dl
+    !end function dichotomie
