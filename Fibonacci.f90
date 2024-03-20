2f0c4046e2b2fcf6b1b65ee50f3f9d7300b17a71
Gaya Héliès <91890427+guy246@users.noreply.github.com>
2024-03-20 10:15:11 +0100

    Update Fibonacci.f90

--- Fibonacci.f90
+++ Fibonacci.f90
@@ -1,27 +1,18 @@
-Soit e une précision donnée. On souhaite calculer une approxiamtion du nombre d'or à e près à l'aide de la suite de Fibonacci :
-Un=Un-1+Un-2
-U0=U1=1
-c'est le rapport (Un+1)/Un qui converge vers le nombre d'or.
-La précision sera défini par l'utilisateur. On se limtera à 50 itérations.
 
 program Fibonacci
-	implicit none
-	!Déclaration des variables
-	real :: U0=1.0, U1=1.0, Un, Un1, Un2, e
-	integer :: i
-	!L'utilisateur rentre la précision
-	print*, "Entrez la précision"
-	read*, e
-	!Calcul de la suite de Fibonacci
-	do i=1,50
-		Un=U0+U1
-		U0=U1
-		U1=Un
-		!Calcul de la précision
-		if (abs((Un1/Un)-1.61803398875)<e) then
-			print*, "Le nombre d'or est ", Un1/Un
-			exit
-		end if
-		Un1=Un
-	end do
+  implicit none
+  integer :: n, i
+  real :: U0, U1, U2, e
+  print*, "Entrez la précision"
+  read*, e
+  U0=1
+  U1=1
+  i=0
+  do while (abs((U1/U0)-1.6180339887)>e .and. i<50)
+    U2=U1+U0
+    U0=U1
+    U1=U2
+    i=i+1
+  end do
+  print*, "Le nombre d'or à ", e, " près est ", U1/U0
 end program Fibonacci
